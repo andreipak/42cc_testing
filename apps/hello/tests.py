2e36f817d0caa4dc2f563ef976804b674d3678ec
Andrei Pak <andrei.pak@gmail.com>
2019-03-28 14:05:18 +0200

    Test for the Person model

--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -1,5 +1,23 @@
 from django.test import TestCase
 
+profile_defaults = {
+    "bio": "Software Developer and\r\nResearcher in a wide variety of" +
+    "\r\napplications and tools",
+    "first_name": "Andrei",
+    "last_name": "Pak",
+    "dob": date(1981, 3, 13),
+    "other_contacts": "LinkedIn Profile",
+    "skype": "pak.andrei",
+    "jabber": "andreipak@42cc.co",
+    "email": "pak.andrei@gmail.com",
+}
+
+
+def create_profile(**kwargs):
+    defaults = profile_defaults.copy()
+    defaults.update(kwargs)
+    return Profile.objects.create(**defaults)
+
 
 class HelloIndexViewTests(TestCase):
     def test_values_exists_on_the_page(self):
@@ -10,3 +28,25 @@ class HelloIndexViewTests(TestCase):
         self.assertContains(response, "Pak")
         self.assertContains(response, "pak.andrei@gmail.com")
         self.assertContains(response, "andreipak@42cc.co")
+
+
+class ProfileModelTests(TestCase):
+
+    def test_fields(self):
+        """
+        test if fields stores valid values
+        """
+        profile = create_profile()
+
+        self.assertEqual(profile.first_name, 'Andrei')
+        self.assertEqual(profile.last_name, 'Pak')
+        self.assertEqual(profile.dob, date(1981, 3, 13))
+        self.assertEqual(profile.bio, 
+            "Software Developer and\r\nResearcher in a wide variety of" +
+            "\r\napplications and tools")
+        self.assertEqual(profile.skype, 'pak.andrei')
+        self.assertEqual(profile.jabber, 'andreipak@42cc.co')
+        self.assertEqual(profile.email, 'pak.andrei@gmail.com')
+        self.assertEqual(profile.other_contacts, 'LinkedIn Profile')
+
+
